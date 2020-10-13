7454c0e347f2ec8fa6d79887434eb3b1ff6da04c
Lynette <lynetteacholah@icloud.com>
2020-10-13 18:09:24 +0300

    First commit

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../src/App.css'
+
+class Contact extends React.Component {
+  render() {
+  return (
+    <div className = 'contact'>
+      <h3>Contact Us</h3>
+      <p>Let us serve you</p>
+      <p>Safaricom : +254 701 803 444</p>
+      <p>Email Address : lynetteacholah@icloud.com</p>
+    </div>
+  )
+  }
+}
+export default Contact
