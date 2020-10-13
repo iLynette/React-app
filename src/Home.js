22b61df2ace830f0ee7297a4060dfde602df8001
Lynette <lynetteacholah@icloud.com>
2020-10-13 22:04:13 +0300

    Added css

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
   return (
     <div className = "ten">
-      <h2>About us</h2>
+      <h2>About us</h2><br/>
       <p>We offer the best foods in town</p>
       <p>We offer the best services in a cool environment with very friendly staff members</p>
       <p>Foods may be served with fresh juice</p>
--- src/Menu.js
+++ src/Menu.js
@@ -6,19 +6,31 @@ class Menu extends React.Component {
   return (
     <div className = "menu">
       <div className = 'main'>
-        menu;
+        Our Menu
       </div>
       <div className = 'food'>
         <figure>
           <img src = 'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
-          <figcaption>VEgetable lot, Ksh.1500</figcaption>
+          <figcaption>Vegetable lot, Ksh.1500</figcaption>
         </figure>
         <figure>
-          <img src = 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
+          <img src = 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
           <figcaption>Vegetable salad, Ksh.1050</figcaption>
         </figure>
         <figure>
           <img src = 'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
+          <figcaption>Grilled steak with vegetables, Ksh.1950</figcaption>
+        </figure>
+        <figure>
+          <img src = 'https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
+          <figcaption>VEgetable lot, Ksh.1500</figcaption>
+        </figure>
+        <figure>
+          <img src = 'https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
+          <figcaption>Vegetable salad, Ksh.1050</figcaption>
+        </figure>
+        <figure>
+          <img src = 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt="food"></img>
           <figcaption>Grilled steak with vegetables, Ksh.1950</figcaption>
         </figure>
       </div>
